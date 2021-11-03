1ea47e3fb826c57e34dad87d3430632e592ada54
farah-ebnhadjali <farah.bhadjali@gmail.com>
2021-11-03 01:21:44 +0100

    Challenge task

--- app/src/main/java/com/farah/affirmations/MainActivity.kt
+++ app/src/main/java/com/farah/affirmations/MainActivity.kt
@@ -2,6 +2,7 @@ package com.farah.affirmations
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.farah.affirmations.adapter.ItemAdapter
 import com.farah.affirmations.data.Datasource
@@ -17,6 +18,10 @@ class MainActivity : AppCompatActivity() {
 
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
         recyclerView.adapter = ItemAdapter(this, myDataset)
+
+        //challenge task
+        val gridLayoutManager = GridLayoutManager(applicationContext, 3, GridLayoutManager.VERTICAL, false)
+        recyclerView.layoutManager = gridLayoutManager
 
         // Use this setting to improve performance if you know that changes
         // in content do not change the layout size of the RecyclerView
